9e416e4d12137dad2503e444745ac42124e009c7
Hansol <losnah24@gmail.com>
2023-06-01 12:36:24 +0900

    불필요한 초기 파일 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -24,7 +22,5 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     OrdersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
