f6b44cf58c29d97eb3a9cad3ab5e866e1e2fd94e
Hansol <losnah24@gmail.com>
2023-06-01 16:49:42 +0900

    Mocking 테스트 코드 작성을 위한 세팅

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { Repository } from 'typeorm';
+import { OrderEntity } from './entities/order.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  let repository: Repository<OrderEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: getRepositoryToken(OrderEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    repository = module.get<Repository<OrderEntity>>(
+      getRepositoryToken(OrderEntity),
+    );
   });
 });
