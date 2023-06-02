3adffdd813308ca1c3deb10f0713185ef8c12ddb
Hansol <losnah24@gmail.com>
2023-06-02 13:14:42 +0900

    Add: Logging Interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './common/all-exception.filter';
+import { HttpLoggingInterceptor } from './common/http-logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   const httpAdapter = app.get(HttpAdapterHost);
 
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.useGlobalInterceptors(new HttpLoggingInterceptor());
 
   app.useGlobalPipes(
     new ValidationPipe({
